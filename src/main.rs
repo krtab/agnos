b967fee4c8bae8f3809d19a21d185038a2fcdf1c
Arthur Carcano <53921575+krtab@users.noreply.github.com>
2021-11-28 00:39:08 +0100

    Allow rsa private key on multiple lines

--- src/main.rs
+++ src/main.rs
@@ -72,7 +72,8 @@ impl TryInto<ProcessedConfigAccount> for TomlOps {
     type Error = eyre::Error;
 
     fn try_into(self) -> Result<ProcessedConfigAccount, Self::Error> {
-        let pem_encoded = base64::decode(self.private_key)?;
+        let pv_key_no_whitespace : Vec<u8> = self.private_key.split_whitespace().flat_map(|s| s.bytes()).collect();
+        let pem_encoded = base64::decode(pv_key_no_whitespace )?;
         let private_key = openssl::rsa::Rsa::private_key_from_pem(&pem_encoded)?.try_into()?;
         Ok(ProcessedConfigAccount {
             email: self.email,
